8b061b51136e392834c3d4b171ae340b041a2f89
yoonsujin <yoonsujinn@naver.com>
2024-07-28 14:48:33 +0900

    qe

--- calc.js
+++ calc.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let calWindow = document.querySelector('.calWindow')
     let grayText = document.querySelector('.grayText')
     let grayNUM;
-    let resultNum = 0;
+    let resultNum ;
     let calcState;
 tds.forEach(td => {
     td.addEventListener('click',function(){
@@ -15,7 +15,11 @@ tds.forEach(td => {
         if(tdAttr == 0 || tdAttr ==1 || tdAttr ==2 || tdAttr ==3 || 
         tdAttr ==4 || tdAttr ==5 || tdAttr ==6
         ||tdAttr ==7 || tdAttr ==8 || tdAttr ==9 ||tdAttr =='.') {
+            if(calcState =='equals') { 
+                windowReset()
+            } 
             windowCall(tdAttr)
+
         
         }else if (tdAttr == 'reset') {
             windowReset();
@@ -39,6 +43,7 @@ function keyPut() {
     let keyCondition1 = (event.keyCode > 47 && event.keyCode <58) || (event.keyCode > 95 && event.keyCode <106 || (event.keyCode ==190) || (event.keyCode ==110)) ;
         if(keyCondition1){
             windowCall(event.key)
+
         }else if(event.keyCode ==27) { //esc키 클릭
             windowReset();
         }else if(event.keyCode ==107) { //+ 키 클릭
@@ -58,21 +63,27 @@ function windowReset() {
     inNum.innerHTML ='';
     grayText.innerHTML ='';
     resultNum = 0;
+    calcState = undefined;
+
+
 }
 
 // 결과값 출력 함수
 function resultPrint() {
     console.log('결과값 출력 함수 실행!!!!!!!!!!!!!!!')
-    console.log(resultNum)
-
+    if (calcState =='plus') {
+        plusPlay() 
+        calcState = undefined;
+    }
+    grayText.innerHTML ='';
     inNum.innerHTML = resultNum;
-
-
+    resultNum = 0;
+    calcState ='equals';
+    // console.log(calcState)
 }
-
 //인풋값 가져와서 숫자로 형변환 시켜주는 함수 
 function inputTrans() {
-    console.log('형변환함수 시작!!');
+    // console.log('형변환함수 시작!!');
     num_1 = inNum.innerHTML;
     num_1 = Number(num_1);
     return num_1;
@@ -83,24 +94,23 @@ function plusPlay() {
     let num_1 = inputTrans();
     grayTextfunc('plus');
     calcState = 'plus';
-
-    console.log (num_1)
-    console.log (typeof num_1)
-
-
-    resultNum = resultNum + num_1;
+    inNum.innerHTML = resultNum;
     inNum.innerHTML ='';
-        if(event.keyCode ==13) { //enter클릭
-            resultNum = resultNum + num_1;
-        }
+    resultNum = resultNum + num_1;
+    console.log(resultNum, 'resultNum 값')
+
 
 
 }
 
 function grayTextfunc(symbol){
-    console.log('그레이텍스트 함수 시작!');
-        if (symbol == 'plus') {
-            grayText.innerHTML = inNum.innerHTML +'+'
+    // console.log('그레이텍스트 함수 시작!');
+    
+        
+        if( inNum.innerHTML == "") {
+            
+        }else if (symbol == 'plus') {
+            grayText.innerHTML = inNum.innerHTML+'+'
         }
     }
 });
